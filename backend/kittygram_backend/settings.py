dcb157cdd4c931d55dac29f72cd2ba14f660dd45
Anzor Kvachantiradze <anzor.green@gmail.com>
2024-12-18 12:05:14 +0100

    5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,8 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = os.getenv('SECRET_KEY', '')
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 DEBUG = True
-
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', 'kotikkotik.zapto.org', '51.250.104.164']
+#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
